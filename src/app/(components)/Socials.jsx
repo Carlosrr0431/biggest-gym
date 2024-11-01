8b827586debe6a7c3e34f44f1b034e33d502a50e
Carlos RR <carlos.facundo.rr@gmail.com>
2024-11-01 19:28:32 -0300

    as

--- src/app/(components)/Socials.jsx
+++ src/app/(components)/Socials.jsx
@@ -23,11 +23,11 @@ const Socials = () => {
 
     {/* <Link href={''} className="duration-300 transition-all hover:scale-110  focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85]"> <Image src={Facebook} width={10} height={10} alt="" /> </Link> */}
 
-    <Link href={'https://www.instagram.com/biggest.gym.online?igsh=Ym9jdzZmdnNocjI3'} target="_blank" className="duration-300 transition-all hover:scale-110  focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85]"> <Image src={Instagram} width={20} height={20} alt="" /> </Link>
-    <Link href={'https://www.tiktok.com/@biggestgym?_t=8q9cnqemPG2&_r=1'} target="_blank"  className="duration-300 transition-all hover:scale-110  focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85]"> <Image src={Tiktok} width={20} height={20} alt="" /> </Link>
-    <Link href={'https://youtube.com/@biggestgym8558?si=YMZ1_-DKyVvRQ5SP'} target="_blank"  className="duration-300 transition-all hover:scale-110  focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85]"> <Image src={Youtube} width={20} height={20} alt="" /> </Link>
+    <Link href={'https://www.instagram.com/biggesttraining?igsh=ZXYxMTF5MmdmN2lw'} target="_blank" className="duration-300 transition-all hover:scale-110  focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85]"> <Image src={Instagram} width={20} height={20} alt="" /> </Link>
+    <Link href={'https://www.tiktok.com/@biggestgym?_t=8q9cnqemPG2&_r=1'} target="_blank" className="duration-300 transition-all hover:scale-110  focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85]"> <Image src={Tiktok} width={20} height={20} alt="" /> </Link>
+    <Link href={'https://youtube.com/@biggestgym8558?si=YMZ1_-DKyVvRQ5SP'} target="_blank" className="duration-300 transition-all hover:scale-110  focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85]"> <Image src={Youtube} width={20} height={20} alt="" /> </Link>
     {/* <Link href={''} className="duration-300 transition-all hover:scale-110  focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85]"> <Image src={Telegram} width={20} height={20} alt="" /> </Link> */}
-    <Link href={'https://wa.me/+543876364202?text=Quiero mas info...'} target="_blank"  className="duration-300 transition-all hover:scale-110 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85]"> <Image src={WhatsApp} width={20} height={20} alt="" /> </Link>
+    <Link href={'https://wa.me/+543876364202?text=Quiero mas info...'} target="_blank" className="duration-300 transition-all hover:scale-110 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85]"> <Image src={WhatsApp} width={20} height={20} alt="" /> </Link>
   </div>;
 };
 
