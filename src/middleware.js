34cd41472bc011d162cebb1169f79483eec457a3
Carlos RR <carlos.facundo.rr@gmail.com>
2024-11-01 18:21:52 -0300

    mensaje 2

--- src/middleware.js
+++ src/middleware.js
@@ -1,45 +1,45 @@
 import { withAuth } from "next-auth/middleware";
 
 export default async function middleware(req, event) {
-  // const authMiddleware = withAuth({
-  //   pages: {
-  //     signIn: `/login`,
-  //   },
+  const authMiddleware = withAuth({
+    pages: {
+      signIn: `/login`,
+    },
 
-  //   callbacks: {
-  //     async authorized({ token }) {
-  //       if (
-  //         req.nextUrl.pathname == "/dashboard/administrador" &&
-  //         token?.role == "admin"
-  //       ) {
-  //         return true;
-  //       } else if (
-  //         req.nextUrl.pathname == "/dashboard/user" &&
-  //         (token?.role == "admin" || token?.role == "user admin")
-  //       ) {
-  //         return true;
-  //       } else if (
-  //         req.nextUrl.pathname == "/" &&
-  //         (token?.role == "member" ||
-  //           token?.role == "admin" ||
-  //           token?.role == "user admin")
-  //       ) {
-  //         return true;
-  //       } else if (
-  //         req.nextUrl.pathname == "/user" &&
-  //         (token?.role == "member" ||
-  //           token?.role == "admin" ||
-  //           token?.role == "user admin")
-  //       ) {
-  //         return true;
-  //       } else return false;
-  //     },
-  //   },
-  // });
+    callbacks: {
+      async authorized({ token }) {
+        if (
+          req.nextUrl.pathname == "/dashboard/administrador" &&
+          token?.role == "admin"
+        ) {
+          return true;
+        } else if (
+          req.nextUrl.pathname == "/dashboard/user" &&
+          (token?.role == "admin" || token?.role == "user admin")
+        ) {
+          return true;
+        } else if (
+          req.nextUrl.pathname == "/" &&
+          (token?.role == "member" ||
+            token?.role == "admin" ||
+            token?.role == "user admin")
+        ) {
+          return true;
+        } else if (
+          req.nextUrl.pathname == "/user" &&
+          (token?.role == "member" ||
+            token?.role == "admin" ||
+            token?.role == "user admin")
+        ) {
+          return true;
+        } else return false;
+      },
+    },
+  });
 
-  // return authMiddleware(req, event);
+  return authMiddleware(req, event);
 }
 
-// export const config = {
-//   matcher: ["/dashboard/administrador", "/dashboard/user", "/user"],
-// };
+export const config = {
+  matcher: ["/dashboard/administrador", "/dashboard/user", "/user"],
+};
