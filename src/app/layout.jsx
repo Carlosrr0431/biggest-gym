ac3fecc5d0fddbb116db9e1224e9c0ae8543787f
Nadu <carlos.facundo.rr@gmail.com>
2025-04-16 22:26:37 -0300

    gracias Dios

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -15,35 +15,23 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
-    // <html lang="es">
-    //   <body className="montserrat overflow-y-hidden">
-    //     <Providers>
-    //       <Suspense>
-    //         <AppWrapper>
-    //           <Nav />
-    //           <Header />
-
-    //           <main
-    //             className={`page  text-white bg-cover bg-no-repeat font-sora overflow-y-scroll w-full h-full overflow-x-hidden `}
-    //           >
-
-    //             {children}
-    //             <Toaster position="bottom-center" richColors />
-
-    //             <div className="fixed xl:right-0 xl:bottom-0 xl:z-50 md:mb-8 md:mr-6">
-
-    //               <WhatsApp />
-
-    //             </div>
-
-    //           </main>
-    //         </AppWrapper>
-    //       </Suspense>
-    //     </Providers>
-    //   </body>
+    // <html lang="en">
+    //   <body>{children}</body>
     // </html>
+    <html lang="es">
+      <body className="montserrat">
+        <Providers>
+          <Suspense>
+            <AppWrapper>
+              <main
+                className={` `}
+              >
+                {children}
+              </main>
+            </AppWrapper>
+          </Suspense>
+        </Providers>
+      </body>
+    </html>
   );
 }
